72e91c29a50ced9aa0db739db710d465a0cbd06f
Solange <sol.cer.87@gmail.com>
2019-08-03 12:43:42 +0100

    Initial commit

---
+++ mjfood/urls.py
@@ -0,0 +1,35 @@
+"""
+Definition of urls for mjfood.
+"""
+
+from datetime import datetime
+from django.urls import path
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from app import forms, views
+
+# https://stackoverflow.com/a/51635038
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls import url
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login/',
+         LoginView.as_view
+         (
+             template_name='app/login.html',
+             authentication_form=forms.BootstrapAuthenticationForm,
+             extra_context=
+             {
+                 'title': 'Log in',
+                 'year' : datetime.now().year,
+             }
+         ),
+         name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('admin/', admin.site.urls),
+    url(r'^app/static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
